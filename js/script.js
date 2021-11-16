87c306f65973259498a9608fe0d9e7d0e3e9de08
christopherzito <christopherzito11@gmail.com>
2021-11-16 10:26:28 +0100

    update switch from users

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ var wApp = new Vue (
                         timeSend:"11:54 11/09/2001",
                         },
                         {
-                        message:"messaggio da scrivere",
+                        message:"messaggio da scrivere Michele",
                         whoSend:"owner",
                         timeSend:"11:54 11/09/2001",
                         },
@@ -29,7 +29,7 @@ var wApp = new Vue (
                     ],
                 },
                  //oggetto 2
-                 {
+                {
                     nome: "Fabio",
                     avatar: "img/avatar_2.jpg",
                     lastAcces:"11/09/2001",
@@ -41,7 +41,7 @@ var wApp = new Vue (
                         timeSend:"11:54 11/09/2001",
                         },
                         {
-                        message:"messaggio da scrivere",
+                        message:"messaggio da scrivere Fabio",
                         whoSend:"owner",
                         timeSend:"11:54 11/09/2001",
                         },
@@ -53,7 +53,7 @@ var wApp = new Vue (
                     ],
                 },
                  //oggetto 3
-                 {
+                {
                     nome: "Samuele",
                     avatar: "img/avatar_3.jpg",
                     lastAcces:"11/09/2001",
@@ -65,7 +65,7 @@ var wApp = new Vue (
                         timeSend:"11:54 11/09/2001",
                         },
                         {
-                        message:"messaggio da scrivere",
+                        message:"messaggio da scrivere Samuele",
                         whoSend:"owner",
                         timeSend:"11:54 11/09/2001",
                         },
@@ -77,7 +77,7 @@ var wApp = new Vue (
                     ],
                 },
                  //oggetto 4
-                 {
+                {
                     nome: "Luisa",
                     avatar: "img/avatar_4.jpg",
                     lastAcces:"11/09/2001",
@@ -89,7 +89,7 @@ var wApp = new Vue (
                         timeSend:"11:54 11/09/2001",
                         },
                         {
-                        message:"messaggio da scrivere",
+                        message:"messaggio da scrivere Luisa",
                         whoSend:"owner",
                         timeSend:"11:54 11/09/2001",
                         },
@@ -102,6 +102,49 @@ var wApp = new Vue (
                 },
                 
             ],
+            display:"",
+            newMessage:"",
+        },
+        methods:{
+            changeChat(whoChat){
+                this.utenti.forEach(variabile => {
+                    return variabile.visible = false;
+                });
+
+                this.utenti[whoChat].visible = true;
+            },
+            sendNewMessage(were){
+                console.log(were);//indica la chat in cui sto scrivendo
+                console.log(this.newMessage, "hai");
+                let newChat = {
+                    message: this.utenti.chat = this.newMessage,
+                    whoSend: "owner",
+                    timeSend: "11:54 11/09/2002"
+                };
+                console.log(newChat);
+                /* this.utenti[were].chat.push(newChat);*/
+                /* this.newMessage = ""; */ 
+            }
+
         },
     }
 );
+/*
+ addObj(){
+    //creazione nuovo oggetto da inserire nell'array
+   
+    if(this.newTextObj != "" && this.newTextObj.length > 4){
+        let newObj = {
+        text: this.object.text = this.newTextObj,
+        done: this.object.done = false,
+        };
+        // console.log("text: ",this.newTextObj);
+        // console.log(newObj);
+
+        this.object.push(newObj)
+        //reset testo input
+        this.newTextObj = "";
+    }
+    
+}, 
+*/
